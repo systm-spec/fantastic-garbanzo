559bf1190eb176756e83b8a5e6c9ead251028d96
PascalData <passi@bertramweb.de>
2024-10-25 15:02:21 +0200

    set first frame
    integrated first frame with a pad 20 on x and y

--- app.py
+++ app.py
@@ -1,15 +1,22 @@
-import customtkinter
+import customtkinter as ctk
 
-customtkinter.set_appearance_mode("dark")
-app = customtkinter.CTk()
+ctk.set_appearance_mode("dark")
+app = ctk.CTk()
 
+app.geometry("700x900")
+app.grid_columnconfigure(0, weight=1)
+app.configure(padx=20, pady=20)
 
+class_frame = ctk.CTkFrame(master=app, width=200, height=400, border_width=1,
+                           border_color="white")
+
+class_frame.grid(row=0, column=0, sticky="EW")
 
 
 def button_event():
     print("button pressed")
 
-button = customtkinter.CTkButton(app, text="CTkButton", command=button_event)
+button = ctk.CTkButton(app, text="CTkButton", command=button_event)
 
 
 
