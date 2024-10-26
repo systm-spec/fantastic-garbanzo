83e26399c448484deb916403bd6fa51c9eed9736
HyBRiZx420 <mitch.hermanos@web.de>
2024-10-26 17:41:23 +0200

    Saturday Update II
    1. simple refactor
    2: add comments
    3. import json

--- app_new.py
+++ app_new.py
@@ -4,6 +4,7 @@ from customtkinter import CTkToplevel
 import tkinter as tk
 from tkinter import filedialog
 import os
+import json
 
 
 
@@ -20,31 +21,47 @@ parent_frame = ctk.CTkFrame(app, fg_color=None)
 parent_frame.pack(expand=True, fill="both")
 parent_frame.grid_columnconfigure(0, weight=1)
 
+###############
+## VARIABLES ##
+###############
+
+users_dict = {}
 
 ###############
 ## Functions ##
 ###############
+
+
+
+# Dialogue: for opening classlist
 def open_cl():
     label.cl_path = filedialog.askopenfilename(defaultextension=".txt",title="Open classlist", initialdir=r"./assets/classlists")
     if label.cl_path:
+        # if cl_path exists strip filename for label
         stripped_path = label.cl_path.split("/")[-1].split(".")[0]
         label.configure(text=stripped_path if stripped_path else "no file selected")
+        # then start open & render process with cl_path
         render_classlist_users(label.cl_path)
 
+# render fn
 def render_classlist_users(cl_path):
     row_counter = 0
     col_counter = 0
+    # open cl_path
+    # remove linebreaks ("\n") & save to list
     with open(cl_path, "r") as reader:
         users = reader.read().split('\n')
+    users.sort()
+
+    # create & render btn with row_ & col_ counter for auto_grid
     for user in users:
         user_btn = ctk.CTkButton(frame_users,width=24, text=user, corner_radius=14)
         user_btn.grid(column=col_counter, row=row_counter, pady=(5,0))
-        if col_counter > 1:
+        if col_counter < 2:
+            col_counter += 1
+        else:
             row_counter += 1
             col_counter = 0
-        else:
-            col_counter += 1
-
 
 #####################
 ## Classlist-Frame ##
